40f1e7c0f782aa93dd8abd74aab88d527e0ed83b
Sean Ward <sean.ward@libertymutual.com>
2017-05-27 11:40:32 -0400

    Updated SetlementPaymentImpl

--- src/main/java/com/lmig/ci/lmb/gi/dao/SettlementFileRepositoryImpl.java
+++ src/main/java/com/lmig/ci/lmb/gi/dao/SettlementFileRepositoryImpl.java
@@ -8,44 +8,69 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
+
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 @Component
 public class SettlementFileRepositoryImpl {
     private static final Logger logger = LoggerFactory.getLogger(SettlementFileRepositoryImpl.class);
+
     @Autowired
     private SettlementFileRepository        fileRepository;
 
+    //************************************************
     public void writeSettlementFile() throws Exception {
 
-    File paymentFile = ResourceUtils.getFile("src/main/resources/payment_settlement.csv");
-    logger.info("File Found :" + paymentFile.exists());
+        File sourceFile = ResourceUtils.getFile("src/main/resources/payment_settlement.csv");
+        long lSourceChecksum = FileUtils.checksumCRC32(sourceFile);
 
-    // Do these is separate methods - private ..calcCheckSum....verifyCheckSum...createFileFromString...
-    // Calculate Checksum using recevied File...save in variable
-    //FileUtils.checksumCRC32(File ####)
+        String fileContent = FileUtils.readFileToString(sourceFile,"UTF-8");
+        int nSourceHash = fileContent.hashCode();
 
-    String fileContent = FileUtils.readFileToString(paymentFile,"UTF-8");
+        SettlementFile fileInsert = new SettlementFile();
+        fileInsert.setInboundfile(fileContent);
+        fileRepository.save(fileInsert);
 
-    SettlementFile fileInsert = new SettlementFile();
-    fileInsert.setInboundfile(fileContent);
-    fileRepository.save(fileInsert);
+        SettlementFile fileSelect = fileRepository.findOne(1L);
+        int nTargetHash = fileSelect.getInboundfile().hashCode();
+        long lTargetChecksum =  this.getCheckSum(fileSelect.getInboundfile());
 
-    SettlementFile fileSelect = fileRepository.findOne(1L);
+        if ( this.isCheckSumEqual(lSourceChecksum,lTargetChecksum)) {
+            logger.info("Payment File Checksum Validaton Success");
+        }else {
+            logger.info("Payment File Checksum Validaton Failed");
+        }
 
-    /* To Do */
-
-    // POSTGRESQL Query Tool - Just to examine data - no code here
-    // ﻿select convert_from(loread(lo_open(inboundfile::int, x'40000'::int), x'40000'::int),  'UTF8') from settlement_file;
-    // to display contents of TEXT field ( inbound file)
-    //
-    // Now code
-    // Now create Methiod to verify file contents
-    // Create file from filerSelect object....Object to String ...need to figure out
-    // Then String to File....bbbbb
-    // Create checksum - FileUtils.checksumCRC32(File bbbbb)
-    // Comnpare Checksum values - Must Be Equal
+        if ( this.isHashCodeEqual(nSourceHash,nTargetHash )) {
+            logger.info("Payment File HashCode Validaton Success");
+        }else {
+            logger.info("Payment File HashCode Validaton Failed");
+        }
     }
 
+    private boolean isCheckSumEqual(long parmSourceChecksum, long parmTargetCheckSum) {
+        return (parmSourceChecksum == parmTargetCheckSum) ? true : false;
+    }
 
+    private boolean isHashCodeEqual(int parmSourceHashCode, long parmTargetHashCode) {
+        return (parmSourceHashCode == parmTargetHashCode) ? true : false;
+    }
+
+    private Long getCheckSum(final String parmString) throws IOException {
+        long lCheckSum =0;
+        try {
+            File temp = File.createTempFile("payment_settlement", "csv");
+            temp.deleteOnExit();
+            BufferedWriter out = new BufferedWriter(new FileWriter(temp));
+            out.write(parmString);
+            out.close();
+            lCheckSum = FileUtils.checksumCRC32(temp);
+        } catch (IOException e) {
+            throw e;
+        }
+        return lCheckSum;
+    }
 }
